b7891e1159324d95d2f5cbb0b374c11212bc7e02
Robert Farris <Robertbfarris@gmail.com>
2020-12-02 17:07:03 -0600

    Working on a custom CMS restructured the old website with corresponding needed PHP

---
+++ firstprogram.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <h1>First Program</h1>
+    </head>
+
+    <body>
+        
+        <?php
+
+            print "Running my first program in PHP with XAMPP Server."
+
+        ?>
+
+    </body>
+</html>
